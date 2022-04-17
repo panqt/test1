61a2d5c5e3fe3863e352f49b3aebdd0a87e52f02
panqt <1255457995@qq.com>
2022-04-17 18:05:51 +0800

    uuuuuuuuu

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,8 +3,6 @@
  */
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
